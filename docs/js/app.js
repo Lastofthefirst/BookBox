d79add44eceb82ee303135607e7fb80f67c3fb7c
Lastofthefirst <47003973+Lastofthefirst@users.noreply.github.com>
2019-04-04 12:21:00 -0400

    Finalize

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,9 +1,8 @@
 // Framework7 App main instance
-let app  = new Framework7({
+let app = new Framework7({
   root: '#app',
   routes: routes,
-}
-);
+});
 
 let $$ = Dom7;
 
@@ -18,7 +17,7 @@ let purchasedBooks = [];
 
 function loadPurchasedBooks() {
   purchasedBooks.forEach(function(book) {
-    $("#" + book).show();
+      $("#" + book).show();
   });
 }
 
@@ -27,16 +26,17 @@ function loadPurchasedBooks() {
 let none;
 
 // This function toggles the background color and navbar visibility within the bookswipers
-function reader_Screen(cl, vw){
+function reader_Screen(cl, vw) {
   let els = document.getElementsByClassName(cl);
-  for(let i=0; i<els.length; ++i){
-     let s = els[i].style;
-     s.display = s.display==='none' ? 'block' : 'none';
+  for (let i = 0; i < els.length; ++i) {
+      let s = els[i].style;
+      s.display = s.display === 'none' ? 'block' : 'none';
   };
   let view = document.getElementsByClassName(vw);
-  for(let i=0; i<view.length; ++i){
-     let s = view[i].style;
-     s.backgroundColor = s.backgroundColor==='black' ? 'rgb(204, 221, 199)' : 'black';
+  for (let i = 0; i < view.length; ++i) {
+      let s = view[i].style;
+      s.backgroundColor = s.backgroundColor === 'black' ? 'rgb(218, 206, 166)' : 'black';
+      s.paddingTop = s.paddingTop === '0px' ? '64px' : '0px';
   }
 }
 
@@ -46,7 +46,7 @@ function reader_Screen(cl, vw){
 localforage.getItem('ownedBooks').then(function(ownedBooksValue) {
   //console.log(ownedBooksValue);
   if (typeof ownedBooksValue !== 'undefined' && ownedBooksValue !== null) {
-    purchasedBooks = ownedBooksValue;
+      purchasedBooks = ownedBooksValue;
   }
   loadPurchasedBooks();
 }).catch(function(err) {
@@ -54,33 +54,33 @@ localforage.getItem('ownedBooks').then(function(ownedBooksValue) {
 });
 
 //This function gets called each time a book is purchased
-function purchaseBook(bookName){
-  purchasedBooks.push(bookName+'book');
+function purchaseBook(bookName) {
+  purchasedBooks.push(bookName + 'book');
   loadPurchasedBooks();
   //add new array value to localforage
-  localforage.setItem('ownedBooks', purchasedBooks).then(function (value) {
-    //console.log(value);
+  localforage.setItem('ownedBooks', purchasedBooks).then(function(value) {
+      //console.log(value);
   }).catch(function(err) {
-    console.log(err);
+      console.log(err);
   });
 }
 
 //try and write a function for if the box of books is empty
 
 //allows for auto initiating swipers in framework7
-  let swiper = app.swiper.create('.swiper-container', {
-    speed: 400,
-    spaceBetween: 0
-  });
+let swiper = app.swiper.create('.swiper-container', {
+  speed: 400,
+  spaceBetween: 0
+});
 
 //Deletes all purchased books, when called, does not have a confirm button because of current purchaseless format
-function clearBooks(){
+function clearBooks() {
   localforage.clear().then(function() {
-    // Run this code once the database has been entirely deleted.
-    console.log('Database is now empty.');
-}).catch(function(err) {
-    // This code runs if there were any errors
-    console.log(err);
-});
-location.reload(); 
+      // Run this code once the database has been entirely deleted.
+      console.log('Database is now empty.');
+  }).catch(function(err) {
+      // This code runs if there were any errors
+      console.log(err);
+  });
+  location.reload();
 }
